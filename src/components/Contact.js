02d097f6e700c869ced222d7461cb2b6b2a75cf8
m1ggy <46958815+m1ggy@users.noreply.github.com>
2021-08-10 22:57:44 +0800

    fix text scaling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,10 +5,10 @@ export default function Contact() {
   return (
     <div className='contactwrapper' id='contact'>
       <div className='contactText'>
-        <h1 data-aos='fade-down'>Contact</h1>
-        <h3 data-aos='fade-up-right'>Need a website built? I gotchu 💯 🤝.</h3>
-        <h2 data-aos='fade-up-left' style={{ fontSize: '1vw' }}>
-          Send me an email at{' '}
+        <h1 data-aos='fade-down'>contact</h1>
+        <h3 data-aos='fade-up-right'>need a website built? i gotchu 💯 🤝.</h3>
+        <h2 data-aos='fade-up-left' style={{ fontSize: '5vw' }}>
+          send an email at{' '}
           <a href='mailto:miguel.buising1@gmail.com'>
             miguel.buising1@gmail.com
           </a>
