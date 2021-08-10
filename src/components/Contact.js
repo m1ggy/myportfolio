67d503ab7945b81106850eecc258ce6394dfd739
m1ggy <46958815+m1ggy@users.noreply.github.com>
2021-08-10 19:58:17 +0800

    boilerplate changes to index.html and manifest

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
     <div className='contactwrapper' id='contact'>
       <div className='contactText'>
         <h1 data-aos='fade-down'>Contact</h1>
-        <h1 data-aos='fade-up-right'>Need a website built? I gotchu 💯 🤝.</h1>
+        <h3 data-aos='fade-up-right'>Need a website built? I gotchu 💯 🤝.</h3>
         <h2 data-aos='fade-up-left'>
           Send me an email at{' '}
           <a href='mailto:miguel.buising1@gmail.com'>
